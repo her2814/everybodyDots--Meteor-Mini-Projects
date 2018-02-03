e1d3be512b4ec1b35c83ded2344c5ee8d11be6f6
her2814 <her2814@naver.com>
2018-02-03 22:08:42 +0900

    라인차트, 봉차트 추가 및 완성

--- client/myChart.js
+++ client/myChart.js
@@ -25,6 +25,20 @@ Template.myChart.onCreated(function(){
         chart.series[0].setData(rtn);
         chart.redraw();
     };
+
+    self.updateLine = function(){
+        var list = self.colorList;
+        var rtn = [];
+
+        for(var i=0,len=list.length; i<len; i++){
+            rtn.push(Dot.find({color:list[i]}).count())
+        }
+
+        var chart = $('div[name=lineChart]').highcharts();
+        chart.series[0].setData(rtn);
+        chart.series[1].setData(rtn);        
+        chart.redraw();
+    };
 });
 
 Template.myChart.onRendered(function(){
@@ -49,5 +63,36 @@ Template.myChart.onRendered(function(){
                  self.updatePie();
              }
         });
+        var chart = this;
+        Dot.find().observe({
+            changed : function(oldDoc,newDoc){
+                var list = self.colorList;
+                chart.series[0].points[_.indexOf(list,oldDoc.color)].update(
+                    Dot.find({color:oldDoc.color}).count()
+                );
+                chart.series[0].points[_.indexOf(list,newDoc.color)].update(
+                    Dot.find({color:newDoc.color}).count()
+                );
+            }
+        });
+    });
+
+    $('div[name=lineChart]').highcharts({
+        title: {text: '모두의 도트 색상 라인 차트'},
+        xAxis: {categories: self.colorList}
+        ,
+        series: [{
+            type: 'line', name: '색상', data:[]
+        },{
+            type:'column',
+            name:'색상',
+            data:[]
+        }]
+    },function(){
+        self.autorun(function(){
+            if(subl.ready()){
+                self.updateLine();
+            }
+        })
     });
 })
